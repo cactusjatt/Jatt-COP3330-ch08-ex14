626783cf5bfabe3656fa8999fac23fac9b8052ba
cactusjatt <75959344+cactusjatt@users.noreply.github.com>
2021-11-29 23:46:46 -0500

    Add files via upload

---
+++ Jatt-COP3330-ch08-ex14/header.h
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Sarim Jatt
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void f(const int x) {
+    cout << x*2 << endl;
+}
+
+void f2(const int y) {
+    cout << y/25 << endl;
+}
---
+++ Jatt-COP3330-ch08-ex14/main.cpp
@@ -0,0 +1,9 @@
+#include "header.h"
+
+int main() {
+  f(3);
+  f2(149);
+  f2(150);
+
+  return 0;
+} 
